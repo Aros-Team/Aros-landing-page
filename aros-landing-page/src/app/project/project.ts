ad2caa07ebc0301ef6313b31e354943025192d62
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2025-11-28 17:17:18 -0500

    css and ts from the component project updated

--- aros-landing-page/src/app/project/project.ts
+++ aros-landing-page/src/app/project/project.ts
@@ -1,11 +1,19 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './project.html',
   styleUrl: './project.css',
 })
 export class Project {
-
+  projects = [
+    {
+      name: "Restaurant order system",
+      images: ["/Proyecto-muestra.jpeg"],
+      description:
+        "This is a software dedicated to managing restaurant orders efficiently. It allows waitstaff to take orders directly from tables using tablets or mobile devices, which are then sent to the kitchen in real-time. The system includes features such as menu management, order tracking, and billing, enhancing the overall dining experience for both customers and staff."
+    }
+  ];
 }
