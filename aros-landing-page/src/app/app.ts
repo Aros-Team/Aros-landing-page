86cb3df6958f75251673cb6e209cac1c7225c59e
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2025-12-03 21:31:37 -0500

    collaborator comp fixed and chat-bot comp created

--- aros-landing-page/src/app/app.ts
+++ aros-landing-page/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Project } from './project/project';
 import { TopBar } from './top-bar/top-bar';
+import { Collaborator } from './collaborator/collaborator';
 
 @Component({
   selector: 'app-root',
-  imports: [ Project, TopBar],
+  imports: [ Project, TopBar, Collaborator],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
